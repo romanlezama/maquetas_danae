2b427cfaff047949e9e5ecf23f55a2995b7f2e8e
romanlezama <romanlezama16@gmail.com>
2016-01-04 13:16:41 -0600

    primer commit para cargar los archivos

---
+++ js/mostrarJson.js
@@ -0,0 +1,25 @@
+
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+$(function() {
+    $('form').submit(function() {
+        $('#result1').text(JSON.stringify($('#maestro').serializeObject()));
+        $('#result2').text(JSON.stringify($('#detalle').serializeObject()));
+        return false;
+    });
+});
